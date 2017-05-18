b09870c5bf161ef6df990a18887e719574d8b76d
Didier Plaindoux <d.plaindoux@free.fr>
2017-05-18 07:15:57 +0200

    Update function-type.swift

--- code/function-type.swift
+++ code/function-type.swift
@@ -6,3 +6,6 @@ func makeIncrementer() -> (Int -> Int) {
 }
 let increment = makeIncrementer()
 increment(7)
+
+// makeIncrementer can also be written in a shorter way:
+let makeincrement = { () in  { (x:Int) in x + 1 } }
